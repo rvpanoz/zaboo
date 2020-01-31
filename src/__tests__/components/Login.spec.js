794db5dbac6ce646e462f0b33e002c1a7395ece2
rvpanoz <rvpanoz@gmail.com>
2020-01-31 07:21:38 +0200

    fix tests

--- src/__tests__/components/Login.spec.js
+++ src/__tests__/components/Login.spec.js
@@ -2,9 +2,12 @@ import React from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { Provider } from "react-redux";
-import Login from "../../components/Login";
+import Login from "components/Login";
+import { ConnectedRouter } from "connected-react-router";
+import { createMemoryHistory } from "history";
 import configureStore from "../../configureStore";
 
+const history = createMemoryHistory();
 const mocks = {
   username: "username@test.com",
   password: "test12345",
@@ -13,11 +16,15 @@ const mocks = {
 
 // Provide initialState for the entire store that the ui is rendered with
 const renderWithRedux = (
-  ui,
+  component,
   { initialState, store = configureStore(initialState) } = {}
 ) => {
   return {
-    ...render(<Provider store={store}>{ui}</Provider>),
+    ...render(
+      <Provider store={store}>
+        <ConnectedRouter history={history}>{component}</ConnectedRouter>
+      </Provider>
+    ),
     // adding `store` to the returned utilities to allow us
     // to reference it in our tests (just try to avoid using
     // this to test implementation details).
